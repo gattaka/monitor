ce6f38e84b3d05884aade840530c740fe0ec4772
Hynek Uhlíř <hynek.uhlir@gmail.com>
2023-11-28 18:59:42 +0100

    Security

--- src/main/java/cz/gattserver/monitor/MonitorController.java
+++ src/main/java/cz/gattserver/monitor/MonitorController.java
@@ -80,6 +80,4 @@ public class MonitorController {
 	String backupDiskMount() throws IOException, InterruptedException {
 		return Console.executeCommand(scriptsPath + backupDiskMountScript);
 	}
-
-
 }
